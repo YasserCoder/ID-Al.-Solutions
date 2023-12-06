2757fa0ef4643083cbb5690a53bbc667430dec29
yasser <yasser.laiss01@gmail.com>
2023-12-06 10:47:04 +0100

    Uploading Project

---
+++ README.md
@@ -0,0 +1,1 @@
+"# ID-Al.-Solutions" 
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/*.html'],
+  theme: {
+    extend: {
+      colors:{
+        body: '#131414',
+        primary:{
+          100: '#217191',
+          200: '#185D78',
+          700: '#204959',
+        },
+        secondary:{
+          100: '#C7A48B',
+          200: '#A97957',
+        }  
+  
+      },
+      fontFamily:{
+        main: ['Montserrat'],
+      },
+      gridTemplateColumns:{
+        '250' : 'repeat(auto-fill, minmax(250px, 1fr));',
+        'percentage' : '48% 48%' ,
+        
+      }
+    },
+    container: {
+      center: true,
+      padding: '15px',
+    },
+    
+    
+    
+  },
+  plugins: [],
+}
+
